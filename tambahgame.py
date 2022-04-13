2bc58a58df9102ba97d9c74de4ae07d74f6c0723
raisyanabila <102876016+raisyanabila@users.noreply.github.com>
2022-04-14 01:21:38 +0700

    Update tambahgame.py

--- tambahgame.py
+++ tambahgame.py
@@ -31,30 +31,3 @@ def TambahGame() :
 
 # YANG KURANG : BELUM ADA GAME ID NYA
 print(TambahGame())
-
-
-'''
-    if (inputGame == " ") or (inputKategori == " ") or (inputTahunRilis == " ") or (inputHarga == " ") or (inputStok == " ") :
-        print("Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.")
-        TambahGame()
-    else :
-        print("Selamat! Berhasil menambahkan game", inputGame)
-        arrayGame = [" " for i in range (5)]
-        for i in range (1) :
-            for j in range(1) :
-                arrayGame[0] += inputGame
-                arrayGame[1] += inputKategori
-                arrayGame[2] += inputTahunRilis
-                arrayGame[3] += inputHarga
-                arrayGame[4] += inputStok
-        return arrayGame
-
-print(TambahGame())'''
-
-
-
-
-'''
-gamebaru = [inputGame, ',', inputKategori, ',', inputTahunRilis, ',', inputHarga, ',' ,inputStok]
-file1 = open('/Users/raisyanabilat/Documents/College Things/DASPRO/TUGAS BESAR/TUGAS BESAR/Program/game.csv', 'w')
-file1.writelines(gamebaru)'''
