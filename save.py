8665f8f9ec8eea81eac1b9f330b17f60069b7748
egi <16521433@mahasiswa.itb.ac.id>
2022-04-15 01:23:35 +0700

    update

--- save.py
+++ save.py
@@ -1,11 +1,32 @@
 import os
 
 def save():
+    from program_binomo import userDs, gameDs, riwayatDs, kepemilikanDs
+    ds = ["game","kepemilikan","riwayat","user"]
     dir = input("Masukkan nama folder penyimpanan")
     if not os.path.exists(dir):
         os.makedirs(dir)
-    os.chdir(dir)
+        os.chdir(dir)
+    else:
+        os.chdir(dir)
+        for type in ds:
+            os.remove(type + ".csv")
+
     print("Saving...")
+
+    for type in ds:
+        with open(type + ".csv", "x") as f:
+            matrix_name = type + "Ds"
+            matrix = globals()[matrix_name]
+            for row in matrix:
+                str = ""
+                for word in row:
+                    str += word
+                f.write(str +"\n")
+        
+
+if __name__ == "__main__":
+    save()
     
 
         
