db35aae038a5b10650810856de1a6ad49d44612d
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-16 06:30:30 +0700

    Update Spek F07 - Listing Game di Toko

--- list_game_toko.py
+++ list_game_toko.py
@@ -1,9 +1,9 @@
 # Spesifikasi Program F07 - Listing Game di Toko Berdasarkan ID, Tahun Rilis dan Harga
 import module
 
-def list_game_toko(my_mtrx):
+def list_game_toko(store_mtrx):
     # Prosedur untuk list game di toko berdasarkan ID atau Tahun Rilis atau Harga
-    # Input: my_mtrx       : array of array of str
+    # Input: store_mtrx       : array of array of str       ( Data dari file "game.csv" )
 
     # KAMUS LOKAL
     # scheme  : str
@@ -12,14 +12,14 @@ def list_game_toko(my_mtrx):
     scheme = str(input('Skema sorting: '))
 
     if scheme == '':
-        module.matrix_print(my_mtrx, [0,1,2,3,4,5])
+        module.matrix_print(store_mtrx, [0,1,2,3,4,5])
     elif scheme == 'harga+':
-        module.matrix_print(module.ascend_sort(my_mtrx,4), [0,1,2,3,4,5])
+        module.matrix_print(module.ascend_sort(store_mtrx,4), [0,1,2,3,4,5])
     elif scheme == 'harga-':
-        module.matrix_print(module.descend_sort(my_mtrx,4), [0,1,2,3,4,5])
+        module.matrix_print(module.descend_sort(store_mtrx,4), [0,1,2,3,4,5])
     elif scheme == 'tahun+':
-        module.matrix_print(module.ascend_sort(my_mtrx,3), [0,1,2,3,4,5])
+        module.matrix_print(module.ascend_sort(store_mtrx,3), [0,1,2,3,4,5])
     elif scheme == 'tahun-':
-        module.matrix_print(module.ascend_sort(my_mtrx,3), [0,1,2,3,4,5])
+        module.matrix_print(module.ascend_sort(store_mtrx,3), [0,1,2,3,4,5])
     else:
         print('Skema sorting tidak valid')
