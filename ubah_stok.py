bae0f3925d07d28292392763aeb21f29b75b683d
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-16 06:26:28 +0700

    Update Spek F06 - Mengubah Stok Game di Toko

--- ubah_stok.py
+++ ubah_stok.py
@@ -3,8 +3,19 @@ import module
 
 def ubah_stok(game_stocks):
     # Fungsi untuk mengubah stok game
-    # Input : game_stocks (array of array of str)
+    # Input : game_stocks           : array of array of str     ( Data dari file "game.csv" )
     # Output: matrix game_stocks yang sudah diubah
+
+    # KAMUS LOKAL
+    # game_id           : str
+    # amount            : int       ( Jumlah stok ingin diubah )
+    # found             : bool
+    # num_of_games      : int       ( Panjang matriks game_stocks )
+    # idx               : int       ( Untuk pengulangan )
+    # name              : str       ( Nama game yang mau dibeli )
+    # chosen_game_idx   : int       ( Indeks yang menyimpang data game yang ingin diubah stoknya )
+
+    # ALGORITMA
     game_id = str(input('Masukkan ID game: '))
     amount = int(input('Masukkan jumlah'))
 
@@ -14,15 +25,16 @@ def ubah_stok(game_stocks):
 
     while not found and idx < num_of_games:
         if game_stocks[1] == game_id:
-            name = game_stocks[1]
-            curr_amount = int(game_stocks[5]) - amount
+            chosen_game_idx = idx
             found = True
     
+    curr_amount = int(game_stocks[chosen_game_idx][5])
     if found:
         if curr_amount >= 0:
-            print(f'Stok game {name} berhasil dikurangi. Stok sekarang: {curr_amount}')
+            game_stocks[chosen_game_idx][5] = curr_amount - amount
+            print(f'Stok game {game_stocks[chosen_game_idx][1]} berhasil dikurangi. Stok sekarang: {game_stocks[chosen_game_idx][5]}')
         else:
-            print(f'Stok game {name} gagal dikurangi karena stok kurang. Stok sekarang: {game_stocks[5]} (<{amount})')
+            print(f'Stok game {game_stocks[chosen_game_idx][1]} gagal dikurangi karena stok kurang. Stok sekarang: {curr_amount} (<{amount})')
     else:
         print('\n Tidak ada game dengan ID tersebut')
 
