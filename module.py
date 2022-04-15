15b7920a07ffc7de55c47074f9bcbceed6d820b9
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-15 13:50:49 +0700

    Updating Module.py

--- module.py
+++ module.py
@@ -104,12 +104,60 @@ def descend_sort(my_mtrx,data_idx):
             check += 1
     return temp_mtrx
 
-# def matrix_print(my_mtrx):
-#     # Fungsi untuk print matrix menjadi suatu tabel
-#     # Input: my_mtrx    : array of array of string
-#     # Output: String    (Bentuk tabel)
+def max_length(my_mtrx, header_idx):
+    # Fungsi untuk mencari jumlah karakter dari string  terpanjang di elemen matrix my_mtrx
+    # Input : my_mtrx       : array of array of str
+    #         header_idx    : int                       (indeks dari bagian elemen yang ingin dicari)
+    # Output: length_bound  : int
 
-#     # KAMUS LOKAL
-#     #
+    # KAMUS LOKAL
+    # elements  : array of string
 
-#     # ALGORITMA
+    # ALGORITMA
+    length_bound = 0
+    for elements in my_mtrx:
+        if elements[header_idx] > length_bound:
+            length_bound = length(elements[header_idx])
+    return length_bound
+
+def matrix_print(my_mtrx, header_list):
+    # Fungsi untuk print matrix menjadi suatu tabel
+    # Input: my_mtrx    : array of array of str
+    #        header_list: array of int              (List yang menyimpan indeks elemen my_mtrx untuk di print)
+    # Output: String    (Bentuk tabel)
+
+    # KAMUS LOKAL
+    # elements_idx                                : array of str
+    # game_name_char, category_char, price_char   : str
+    # mtrx_length                                 : int
+    # space_game_name, space_category, space_price: str
+    # i, idx                                      : int   (untuk pengulangan)
+
+    # ALGORITMA
+    game_name_char = max_length(my_mtrx, 1) # Maksimum karakter untuk nama game
+    category_char = max_length(my_mtrx, 2)  # Maksumum karakter untuk kategori
+    price_char = max_length(my_mtrx, 4) # Maksimum karakter untuk harga
+    mtrx_length = length(my_mtrx)
+
+    for elements_idx in range(mtrx_length):
+        space_game_name = ''
+        space_category = ''
+        space_price = ''
+
+        for i in range(game_name_char - length(my_mtrx[elements_idx][1])):
+            space_game_name += ' '
+        for i in range(category_char - length(my_mtrx[elements_idx][2])):
+            space_category += ' '
+        for i in range(price_char - length(my_mtrx[elements_idx][4])):
+            space_price += ' '
+
+        print(f'{elements_idx + 1}. {my_mtrx[elements_idx][header_list[0]]} ', end='')
+        for idx in header_list:
+            if idx > 0:
+                print(f'| {my_mtrx[elements_idx][idx]}', end='')
+                if idx == 1:
+                    print(space_game_name, end=' ')
+                elif idx == 2:
+                    print(space_category, end=' ')
+                elif idx == 4:
+                    print(space_price, end=' ')
