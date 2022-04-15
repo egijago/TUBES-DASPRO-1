b7bdf070101a300a345cfb7f40a5b070e3f83aa7
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-15 22:47:57 +0700

    Upload spek F08 - Membeli game

---
+++ buy_game.py
@@ -0,0 +1,52 @@
+# Spesifikasi Program F08 - Membeli Game
+import module
+
+def buy_game(store_mtrx, ownership_mtrx, user_mtrx, user_id):
+    # Prosedur jika user ingin membeli game
+    # Input : store_mtrx        : array of array of str
+    #         ownership_mtrx    : array of array of str
+    #         user_id           : str
+
+    # KAMUS LOKAL
+    # found                                       : bool
+    # game_bought                                 : str
+    # ownership_idx, games_idx, users_idx         : int
+    # store_length, ownership_length, user_length : int
+    # chosen_game_idx, chosen_user_idx            : int
+
+
+    # ALGORITMA
+    game_bought = input('Masukkan ID Game: ')   # ID Game yang dibeli
+    
+    found = False
+    ownership_idx = 0
+    store_length = module.length(store_mtrx)
+    ownership_length = module.length(ownership_mtrx)
+    user_length = module.length(user_mtrx)
+
+    for games_idx in range(store_length):
+        if store_mtrx[games_idx][0] == game_bought:
+            chosen_game_idx = games_idx
+            games_idx = store_length
+    
+    while not found and ownership_idx < ownership_length:
+        if ownership_mtrx[ownership_idx][0] == game_bought and ownership_mtrx[ownership_idx][1] == user_id:
+            found = True
+        ownership_idx += 1
+
+    for users_idx in range(user_length):
+        if user_mtrx[users_idx][0] == user_id:
+            chosen_user_idx = users_idx
+            users_idx = user_length
+
+    if store_mtrx[chosen_game_idx][5] == 0:
+        print('Stok game tersebut sedang habis!')
+    else:
+        if found:
+            print('Anda sudah memiliki Game tersebut')
+        else:
+            if store_mtrx[chosen_game_idx][4] <= user_mtrx[chosen_user_idx][5]:
+                print(f'Game \"{store_mtrx[chosen_game_idx][1]}\" berhasil dibeli!')
+                ownership_mtrx += [store_mtrx[chosen_game_idx][0],user_mtrx[chosen_user_idx][0]]
+            else:
+                print('Saldo anda tidak cukup untuk membeli Game tersebut')
