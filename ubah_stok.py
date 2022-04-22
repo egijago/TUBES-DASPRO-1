f1dc003fac5cff8dbf50a81c934f9fd37f8d9e80
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-22 19:26:08 +0700

    Update Spek F06 - Mengubah Stok Game di Toko

--- ubah_stok.py
+++ ubah_stok.py
@@ -1,6 +1,6 @@
 # Spesifikasi Program F06 - Mengubah Stok Game di Toko
 import module
-data_game = ['G001','Adventure','2022','100000','1']
+
 def ubah_stok(game_stocks):
     # Fungsi untuk mengubah stok game
     # Input : game_stocks           : array of array of str     ( Data dari file "game.csv" )
@@ -17,29 +17,31 @@ def ubah_stok(game_stocks):
 
     # ALGORITMA
     game_id = str(input('Masukkan ID game: '))
-    amount = int(input('Masukkan jumlah: '))
 
     found = False
     num_of_games = module.length(game_stocks)
     idx = 0
+    chosen_game_idx = 0
 
     while not found and idx < num_of_games:
-        if game_stocks[0] == game_id:
+        if game_stocks[idx][0] == game_id:
             chosen_game_idx = idx
             found = True
         idx += 1
-    
-    curr_amount = int(game_stocks[chosen_game_idx][5])
+
+    curr_amount = int(game_stocks[chosen_game_idx][5])  # Stok game yang ada
     if found:
-        if curr_amount >= 0:
-            game_stocks[chosen_game_idx][5] = curr_amount - amount
-            print(f'Stok game {game_stocks[chosen_game_idx][1]} berhasil dikurangi. Stok sekarang: {game_stocks[chosen_game_idx][5]}')
+        amount = int(input('Masukkan jumlah: '))
+        if amount >= 0:
+            game_stocks[chosen_game_idx][5] = curr_amount + amount
+            print(f'Stok game {game_stocks[chosen_game_idx][1]} berhasil ditambah. Stok sekarang: {game_stocks[chosen_game_idx][5]}')
         else:
-            print(f'Stok game {game_stocks[chosen_game_idx][1]} gagal dikurangi karena stok kurang. Stok sekarang: {curr_amount} (<{amount})')
+            if amount >= curr_amount:
+                game_stocks[chosen_game_idx][5] = str(curr_amount + amount)
+                print(f'Stok game {game_stocks[chosen_game_idx][1]} berhasil dikurangi. Stok sekarang: {game_stocks[chosen_game_idx][5]}')
+            else:
+                print(f'Stok game {game_stocks[chosen_game_idx][1]} gagal dikurangi karena stok kurang. Stok sekarang: {curr_amount} (< {-1 * amount})')
     else:
-        print('\n Tidak ada game dengan ID tersebut')
+        print('\nTidak ada game dengan ID tersebut')
 
     return game_stocks
-
-data_game = ubah_stok(data_game)
-print(data_game)
