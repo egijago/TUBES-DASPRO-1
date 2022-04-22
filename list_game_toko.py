12c1dcc2d268bdf324df6973e35ff8737e38bdd8
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-22 20:54:26 +0700

    Update Spek F07

--- list_game_toko.py
+++ list_game_toko.py
@@ -1,6 +1,8 @@
 # Spesifikasi Program F07 - Listing Game di Toko Berdasarkan ID, Tahun Rilis dan Harga
 import module
-
+store_data = [['GAME008','AC Unity','Adventure','2014','430000','0'],
+            ['GAME009','Elder Ring','Adventure','2022','861000','1'],
+            ['GAME008','GoW: Ragnarok','Adventure','2021','1000000','5']]
 def list_game_toko(store_mtrx):
     # Prosedur untuk list game di toko berdasarkan ID atau Tahun Rilis atau Harga
     # Input: store_mtrx       : array of array of str       ( Data dari file "game.csv" )
@@ -20,6 +22,7 @@ def list_game_toko(store_mtrx):
     elif scheme == 'tahun+':
         module.matrix_print(module.ascend_sort(store_mtrx,3), [0,1,2,3,4,5])
     elif scheme == 'tahun-':
-        module.matrix_print(module.ascend_sort(store_mtrx,3), [0,1,2,3,4,5])
+        module.matrix_print(module.descend_sort(store_mtrx,3), [0,1,2,3,4,5])
     else:
         print('Skema sorting tidak valid')
+list_game_toko(store_data)
