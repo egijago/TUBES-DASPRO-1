78958164f18d1f6da57c7a170ea4880de0d375de
Fauzan Ibrahim <fauzan.ibrahim21@gmail.com>
2022-04-23 11:36:07 +0700

    Update Spek F13

--- riwayat.py
+++ riwayat.py
@@ -16,14 +16,16 @@ def riwayat(store_mtrx, ownership_mtrx, user_id, history_mtrx):
     # ALGORITMA
     bought_games = list_game.games_user_bought(store_mtrx, ownership_mtrx, user_id)
 
-    user_buy_history = [['' for i in range(4)]]
+    user_buy_history = []
+    
     for games in bought_games:
         for histories in history_mtrx:
-            if games[0] == histories[0]:
-                user_buy_history += [histories[0],histories[1],histories[2], histories[4]]
+            if games[0] == histories[0] and user_id == histories[3]:
+                user_buy_history += [[histories[0],histories[1],histories[2], '', histories[4]]]
     
-    if user_buy_history == [['', '', '', '']]:
+    if user_buy_history == []:
         print('Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.')
     else:
-        print('Daftar game:')
-        module.matrix_print(user_buy_history,[0,1,2,3])
+        print(user_buy_history)
+        print('Daftar game:',end='')
+        module.matrix_print(user_buy_history,[0,1,2,4])
