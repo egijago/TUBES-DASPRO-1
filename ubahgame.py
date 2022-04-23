c5cdb469d2bdbdc2c01a6f67b7df55b6fe42e79b
raisyanabila <102876016+raisyanabila@users.noreply.github.com>
2022-04-23 22:07:07 +0700

    Update ubahgame.py

--- ubahgame.py
+++ ubahgame.py
@@ -1,11 +1,22 @@
+# F05 - Ubah Game
+
 import module as module
-# data_game = [['id', 'nama', 'kategori', 'tahun rilis', 'harga', 'stok'], 
-#              ['G001', 'BNMO', 'Adventure', '2013', '120000', '2'], 
-#              ['G002', 'Barbie', 'Romance', '2020', '180000', '2']]
 
 def changeGame(gameDs) :
+    # Fungsi changeGame 
+    # Mengubah game pada data
+    # Input : gameDs : array of array of str ( Data dari file "game.csv" )
+    
+    # KAMUS LOKAL
+    # gameId           : str
+    # inputGame        : str
+    # inputKategori    : str
+    # inputTahunRilis  : int
+    # inputHarga       : int
+
+    # ALGORITMA
     data_game = gameDs
-    # global data_game
+    
     gameId = input("Masukkan ID game : ")
     inputGame = input("Masukkan nama game : ")
     inputKategori = input("Masukkan kategori : ")
@@ -13,6 +24,7 @@ def changeGame(gameDs) :
     inputHarga = input("Masukkan harga : ")
 
     while (isIdValid(gameId,data_game) == False) or module.length(gameId) == 0 :
+        # Apabila ID game yang diinput tidak valid (tidak terdapat pada file)
         if (isIdValid(gameId,data_game) == False)  :
             print("Tidak ada game dengan ID tersebut")
             gameId = input("Masukkan ID game : ")
@@ -20,6 +32,8 @@ def changeGame(gameDs) :
             inputKategori = input("Masukkan kategori : ")
             inputTahunRilis = input("Masukkan tahun rilis : ")
             inputHarga = input("Masukkan harga : ")
+            
+         # Apabila tidak memasukkan ID dari game yang ingin diubah
         if module.length(gameId) == 0 :
             print("Mohon masukkan ID game untuk mengubah game")
             gameId = input("Masukkan ID game : ")
@@ -28,7 +42,8 @@ def changeGame(gameDs) :
             inputTahunRilis = input("Masukkan tahun rilis : ")
             inputHarga = input("Masukkan harga : ")
 
-
+            
+    # Mengubah data game
     for i in range (module.length(data_game)) :
         for j in range (1) :
             if data_game[i][j] == gameId :
@@ -44,15 +59,25 @@ def changeGame(gameDs) :
     return data_game
 
 def isIdValid(gameId,data_game) :
+    # Fungsi isIdValid
+    # Mengecek apakah ID game yang diinput valid atau tidak
+    # Input : gameId    : int
+    #         data_game : array of array in int ( Data dari file "game.csv" )
+    
+    # KAMUS LOKAL
+    # sameid : int
+    
+    # ALGORITMA
     sameid = 0
     
+    # Mengecek kemunculan ID dalam data
     for i in range (module.length(data_game)): 
         for j in range (1) : 
             if data_game [i][j] == gameId : 
                 sameid += 1
             else :
                 pass 
-
+    
     if sameid != 0 :
         return True
     else :
