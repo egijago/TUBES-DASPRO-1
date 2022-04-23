7e035041ae33ca48dfa2ae7a2681c915eb43b5bc
raisyanabila <102876016+raisyanabila@users.noreply.github.com>
2022-04-23 21:36:23 +0700

    Update register.py

--- register.py
+++ register.py
@@ -50,7 +50,10 @@ def isUsernameValid(inputUsername) :
     # Mengecek apakah username yang dimasukkan valid dan sesuai ketentuan yang ditetapkan
     # Input : inputUsername : str
     
-    #ALGORITMA
+    # KAMUS LOKAL
+    # CharacterValid : str
+    
+    # ALGORITMA
     validCharacters = ['A', 'a', 'B', 'b', 'C', 'c', 'D', 'd', 'E', 'e', 'F','f', 'G', 'g', 'H', 'h', 'I', 'i', 'J',
                         'j', 'K', 'k', 'L', 'l', 'M', 'm', 'N', 'n', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's',
                         'T', 't', 'U', 'u', 'V', 'v', 'W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z', '_', '-', '0', '1', '2',
@@ -76,6 +79,9 @@ def isAlreadyUsed(inputUsername,data_user) :
     # Mengecek apakah username yang diinput oleh pengguna telah digunakan atau belum
     # Input : inputUsername : str
     #         data_user     : array of array of str ( Data dari file "user.csv" )
+    
+    # KAMUS LOKAL
+    # sameUsername : int
     
     # ALGORITMA
     sameUsername = 0
